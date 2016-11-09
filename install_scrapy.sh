d8c25e5a99925da874a63678b71fa2b62968ed1e
mezhou887 <mezhou887@foxmail.com>
2016-11-09 09:58:34 +0800

    fix bug

--- install_scrapy.sh
+++ install_scrapy.sh
@@ -5,21 +5,21 @@
 sudo apt-get update & apt-get dist-upgrade
 
 # 安装需要的系统软件
-apt-get install python-dev python-pip ssh git
+sudo apt-get install python-dev python-pip ssh git
 
 # 将pip更新到最新版
-python -m pip install --upgrade pip
+sudo python -m pip install --upgrade pip
 
 # 安装python必须的软件
-pip install flask requests redis scrapy scrapyd scrapylib scrapy-redis pybloom beautifulsoup4 xlwt
+sudo pip install flask requests redis scrapy scrapyd scrapylib scrapy-redis pybloom beautifulsoup4 xlwt
 
 # 安装不是很必须的软件
-pip install virtualenv virtualenvwrapper django uwsgi selenium sqlalchemy pymysql fake-useragent 
-apt-get install nginx mongodb-server mongodb-client pymongo scrapy-mongodb
+sudo pip install virtualenv virtualenvwrapper django uwsgi selenium sqlalchemy pymysql fake-useragent 
+sudo apt-get install nginx mongodb-server mongodb-client pymongo scrapy-mongodb
 
 # 更新全部第三方库到最新版本
-pip list --outdated | grep '^[a-z]* (' | cut -d " " -f 1 | xargs pip install -U
+sudo pip list --outdated | grep '^[a-z]* (' | cut -d " " -f 1 | xargs pip install -U
 
 # 导出所有第三方库到requirements.txt
-pip freeze > requirements.txt
+sudo pip freeze > requirements.txt
 
