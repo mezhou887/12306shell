8c0b5516940ab3beefa1f98f94a9e8aad2e33508
Zhou Maoen <mezhou887@foxmail.com>
2017-05-30 02:51:28 +0800

    “更新”

--- parse_station.py
+++ parse_station.py
@@ -11,4 +11,5 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 url='https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9002'
 response=requests.get(url,verify=False)
 stations=re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)',response.text)
-pprint(dict(stations),indent=4)
+#pprint(dict(stations),indent=4)
+pprint(stations)
--- parse_train_list.py
+++ parse_train_list.py
@@ -9,12 +9,13 @@ import datetime
 # 禁用安全请求警告
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
 now = datetime.datetime.now();
+url='https://kyfw.12306.cn/otn/resources/js/query/train_list.js?scriptVersion=1.0';
+response=requests.get(url,verify=False);
+txt=response.text[16:];
+file_object = open('train_list_'+now.strftime('%Y%m%d%H')+'.json', 'w');
+file_object.write(txt);
+file_object.close();
 
-url='https://kyfw.12306.cn/otn/resources/js/query/train_list.js?scriptVersion=1.0'
-response=requests.get(url,verify=False)
-txt=response.text[16:-1]
-file_object = open('train_list_'+now.strftime('%Y%m%d%H')+'.json', 'w')
-file_object.write(txt)
-file_object.close( )
+
+# 解析json数据
